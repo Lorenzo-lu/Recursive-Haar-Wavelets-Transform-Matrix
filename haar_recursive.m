820a610342f41d235420908c523928ea55a713f7
Lorenzo-lu <54776772+Lorenzo-lu@users.noreply.github.com>
2019-10-18 18:51:53 -0500

    Add files via upload

--- haar_recursive.m
+++ haar_recursive.m
@@ -1,7 +1,7 @@
 function [M] = haar_recursive(M,n,mask,pixel)
 % when imput, leave M as []
 % for DMD mask and pixel are 1
-% example: haar([],2,1,1)
+% example: haar_recursive([],2,1,1)
 if n == 0
     M(end + 1,:) = mask(:);
 end
@@ -26,9 +26,9 @@ if n > 0
     mask4(1:pixel,pixel+1:2*pixel) = 0;
     mask4(pixel+1:2*pixel,pixel+1:2*pixel) = mask;
     
-    M = haar(M,n-1,mask1,pixel*2);
-    M = haar(M,n-1,mask2,pixel*2);
-    M = haar(M,n-1,mask3,pixel*2);
-    M = haar(M,n-1,mask4,pixel*2);
+    M = haar_recursive(M,n-1,mask1,pixel*2);
+    M = haar_recursive(M,n-1,mask2,pixel*2);
+    M = haar_recursive(M,n-1,mask3,pixel*2);
+    M = haar_recursive(M,n-1,mask4,pixel*2);
 end
 end
